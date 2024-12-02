89d59afa9001f2b8c616da0d87d2f4bc3472dc7a
CarloDettori <dettoric07@gmail.com>
2024-12-02 16:01:06 +0100

    preassets

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 "use strict"
 const express = require("express");
-const server = express();
+const app = express();
 const PORT = 3000;
 
-server.get("/", (req, res) => { });
-server.listen(PORT, () => { }) 
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.send("Server del mio blog")
+});
+
+
+app.all('*', (req, res) => {
+    res.status(404).send('<h1> ERROR 404, Not Found !</h1>');
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}}`)
+}) 
