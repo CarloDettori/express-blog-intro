1c5d0fb977d27a5072f39016e5bb363e4349f3e0
CarloDettori <dettoric07@gmail.com>
2024-12-02 17:28:59 +0100

    array done

--- server.js
+++ server.js
@@ -16,4 +16,39 @@ app.all('*', (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}}`)
-}) 
+})
+
+const Posts = [
+    { conteggio: 5 },
+    {
+        title: "Ciambellone",
+        content: "Il ciambellone è buono",
+        image: "public/img/ciambellone.jpeg",
+        tags: ["dessert", "dolce", "feste"],
+    },
+    {
+        title: "Cracker di barbabietola",
+        content: "Il cracker di barbabietola è buono",
+        image: "public/img/cracker-barbabietola.jpg",
+        tags: ["snak", "dolce", "salutare"],
+    },
+    {
+        title: "Pane fritto dolce",
+        content: "Il pane fritto dolce è buono",
+        image: "public/img/pane-fritto-dolce.jpg",
+        tags: ["contorno", "salato", "cena"],
+    },
+    {
+        title: "Pasta alla barbabietola",
+        content: "La pasta alla barbabietola è buona",
+        image: "public/img/pasta-barbabietola.jpg",
+        tags: ["primo", "salto", "pranzo"],
+    },
+    {
+        title: "Torta paesana",
+        content: "La torta paesana è buona",
+        image: "public/img/paesana.jpg",
+        tags: ["dessert", "dolce", "tradizionale"],
+    }
+]
+
